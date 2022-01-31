c170530c9c28f2aec6382fc8f120a7ac627e0982
prernasunerha <95451346+prernasunerha@users.noreply.github.com>
2022-01-31 17:13:49 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,116 @@
+//task-4 Task Form Input Validation
+const form = document.querySelector("#addTaskForm");
+
+//task-6 Initialize a new TaskManager
+const task = new TaskManager(0);
+
+//task-4 Task Form Input Validation
+form.addEventListener("submit", (event) => {
+    //alert("Error");
+    const taskName = document.querySelector('#taskName');
+    const status = document.querySelector('#status');
+    const taskDescription = document.querySelector('#taskDescription');
+    const validateAssignedTo = document.querySelector('#assignedTo');
+    const validateDueDate = document.querySelector('#dueDate');
+    let validationFail = 0;
+
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("Task Name :" + taskName.value.length);
+    console.log("Task Description :" + taskDescription.value.length);
+    console.log("Task Assigned To :" + validateAssignedTo.value.length);
+    console.log("Task Due Date :" + validateDueDate.value);
+    console.log("Task Status:" + status.value);
+
+    const clearFormFields = () => {
+      taskName.value = "";
+      taskDescription.value = "";
+      validateAssignedTo.value = "";
+      validateDueDate.value = "";
+      status.value = "In Progress";
+      };
+    
+//Check if the Task Name input value is more than 5 characters.
+    if(taskName.value.length > 5 ){
+       taskName.classList.add("is-valid");
+       taskName.classList.remove("is-invalid");
+    } else {
+        taskName.classList.add("is-invalid");
+        taskName.classList.remove("is-valid");
+        validationFail++;  
+    }
+
+
+//Check if the Task Description input value is more than 5 characters.
+    if(taskDescription.value.length > 5){
+    taskDescription.classList.add("is-valid");
+    taskDescription.classList.remove("is-invalid");
+  } else {
+    taskDescription.classList.add("is-invalid");
+    taskDescription.classList.remove("is-valid");
+    validationFail++;
+
+    }
+
+//Check if the Assigned To value is more than 5 characters.
+    if(validateAssignedTo.value.length > 5){
+        validateAssignedTo.classList.add("is-valid");
+        validateAssignedTo.classList.remove("is-invalid");
+      } else {
+        validateAssignedTo.classList.add("is-invalid");
+        validateAssignedTo.classList.remove("is-valid");
+        validationFail++;
+    }
+
+//Check if the Task Due Date input value is not empty.
+    if (validateDueDate.value) {
+        validateDueDate.classList.add("is-valid");
+        validateDueDate.classList.remove("is-invalid");
+      } else {
+        validateDueDate.classList.add("is-invalid");
+        validateDueDate.classList.remove("is-valid");
+        validationFail++;
+      }
+
+//Check if the Task Status input value is not empty.}
+    if (status.value) {
+        status.classList.add("is-valid");
+        status.classList.remove("is-invalid");
+      } else {
+        status.classList.add("is-invalid");
+        status.classList.remove("is-valid");
+        validationFail++;
+      }
+      
+    if (validationFail > 0) {
+        validationFail = 0;
+        return;
+
+      } else {
+        task.addTask(taskName.value, status.value, validateAssignedTo.value, taskDescription.value, validateDueDate.value);
+        clearFormFields();
+        task.render();
+      }
+});
+
+//task-5 Display the Current Date
+const dateElement =document.querySelector("#date-element");
+let today = new Date();
+const [month, day, year] = [today.getMonth()+1, today.getDate(), today.getFullYear()];
+let dateString = `Current Date: ${day} / ${month} / ${year}`;
+dateElement.innerHTML = dateString;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/taskManager.js
@@ -0,0 +1,77 @@
+//task-7 Display The Tasks
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+    const html = `<li class="list-group-item">
+    <div class="card text-center">
+        <div class="card-header text-primary font-weight-bold">${status}</div>
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${description}</p>
+            <hr>
+            <div class="container">
+                <div class="row">
+                    <div class="col">Assigned to:</div>
+                    <div class="col">Due By: </div>
+                </div>
+                <div class="row">
+                    <div class="col text-muted">${assignedTo}</div>
+                    <div class="col text-muted">${dueDate}</div>
+                </div>
+            </div>
+            <br>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#editTask">Edit</button>  
+            <button type="submit" class="btn btn-outline-danger">Delete</button>
+        </div>
+    </div>
+    </li>`;
+    return html;
+  };
+
+
+//task-6 Creating a Class and Adding Tasks Programmatically
+class TaskManager  {
+    constructor (currentId =0) {
+          this.tasks = [];
+          this.currentId = currentId;
+
+    }
+    addTask(taskName,status,assignedTo,description,dueDate) {
+        const task = {
+            taskName:taskName,
+            status: status,
+            assignedTo: assignedTo,
+            description: description,
+            dueDate:dueDate,
+            id: this.currentId++,
+        };
+        this.tasks.push({task});
+    };
+
+
+//Create the render method
+render() {
+let tasksHtmlList = [];
+for (let i = 0; i < this.tasks.length; i++) {
+    const task1 = this.tasks[i];
+
+    const date = new Date(task1.task.dueDate);
+    const formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    const taskHtml = createTaskHtml(
+      task1.task.taskName,
+      task1.task.description,
+      task1.task.assignedTo,
+      formattedDate,
+      task1.task.status
+    );
+    tasksHtmlList.push(taskHtml);
+  }
+  const tasksHtml = tasksHtmlList.join("\n");
+  const tasksList = document.querySelector("#task-list");
+  tasksList.innerHTML = tasksHtml;
+}
+}
+
+
+
+
